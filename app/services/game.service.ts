61c05a34dc33f29478d5a6bb9ace4f28fc919c90
Levi Fuller <Levi Fuller>
2016-09-19 23:34:02 -0600

    IGDB API Working

--- app/services/game.service.ts
+++ app/services/game.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Headers } from '@angular/http';
 
 const GIANTBOMB_API_KEY: string = "19806379abe6beaf668b2e3a60233e64eb72d04a";
 const BASE_URL: string = "http://www.giantbomb.com/api/game/3030-4725/?api_key=19806379abe6beaf668b2e3a60233e64eb72d04a&format=json";
-const FILTER = "&field_list=genres,name";
-
+const IGB_DB: string = "https://igdbcom-internet-game-database-v1.p.mashape.com/companies/";
+const ZELDA_URL: string = "https://igdbcom-internet-game-database-v1.p.mashape.com/games/?fields=name&limit=10&offset=0&order=release_dates.date%3Adesc&search=zelda"
+const IGDB_KEY = "r4yJxhWysqmshxNldmE4ExKFwONzp1DLu2Vjsna4LbVzRtBXqr";
 @Injectable()
 export class GameService {
     constructor(private _http: Http) {
-
+        
     }
     public getGames() {
-        this._http.get(BASE_URL).subscribe(x => {
+        let header: Headers = new Headers();
+        header.append('X-Mashape-Key', IGDB_KEY);
+        this._http.get(ZELDA_URL, {headers: header}).subscribe(x => {
             console.log(x);
         })
     }
